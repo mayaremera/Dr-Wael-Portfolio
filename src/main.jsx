0e7e3a2526f3082fe457e3703721a70b9b025714
YoussefAshour <youssefashour19@gmail.com>
2026-06-25 02:21:06 +0300

    pushing vercel ananalytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import { clearLegacyLocalCaches } from './data/contentSync'
 import './index.css'
@@ -9,6 +10,7 @@ clearLegacyLocalCaches()
 createRoot(document.getElementById('root')).render(
   <>
     <App />
+    <Analytics />
     <SpeedInsights />
   </>,
 )
